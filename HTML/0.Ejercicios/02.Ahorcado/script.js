266046a97d667ed1b24f7fceb6315ae496f5b647
DPM81Dev <devmalfeito@gmail.com>
2024-01-17 19:28:00 +0100

    rev. 1.1 errores

--- HTML/0.Ejercicios/02.Ahorcado/script.js
+++
@@ -1,5 +0,0 @@
-function convertirMayuscula(){ // funcion para capturar la letra y convertir en mayuscula.
-    letra=Document.getElementById("letraPrueba").value
-    console.log(letra); //mostramos la letra capturada
-    document.getElementById("letraPrueba").value = letra.toUppferCase()
-}
